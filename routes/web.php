001be334d351b95ec9c708e30169c9087774ff87
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-10-16 16:47:26 +0200

    add controller and model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
-
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::get('/train', [TrainController::class, 'index'])->name('train.index');
